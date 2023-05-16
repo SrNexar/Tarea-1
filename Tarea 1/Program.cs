a1d78a6f541ff7ffc40a978a74323c56258cce49
nexar <nexar@LAPTOPN>
2023-05-15 20:49:51 -0500

    Agregar archivos de proyecto.

---
+++ Tarea 1/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la calculadora.");
+
+           
+            Console.Write("Ingresa el primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.Write("Ingresa el segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.WriteLine("Elige una operación:");
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Multiplicación");
+            Console.WriteLine("4. División");
+
+           
+            Console.Write("Opción: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            double resultado = 0;
+
+            
+            switch (opcion)
+            {
+                case 1:
+                    resultado = num1 + num2;
+                    break;
+                case 2:
+                    resultado = num1 - num2;
+                    break;
+                case 3:
+                    resultado = num1 * num2;
+                    break;
+                case 4:
+                    resultado = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida.");
+                    break;
+            }
+
+            
+            Console.WriteLine("El resultado es: " + resultado);
+
+            Console.WriteLine("Presiona cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
